dd5fc9a2cb7df22b6fa145051ff18c82c24e8347
Feynman43 <mohammed.znitar@praguecollege.cz>
2022-06-04 00:33:15 +0200

    First prototype of uncolored output

--- src/main/java/com/med/msgviewer/MessageFormatter.java
+++ src/main/java/com/med/msgviewer/MessageFormatter.java
@@ -1,18 +1,29 @@
 package com.med.msgviewer;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class MessageFormatter {
     public static String formatter(String fileContent) {
 
-        System.out.println(fileContent);
-
         //The split is done based on newline (which is defined differently in each OS)
         //regex takes that into consideration to ensure cross-platform compatibility
-        String [] fields = fileContent.split("(\\r\\n|\\r|\\n)");
-        // fields = "4231", "Java Programming", "1000.00"
+        //fields takes a string and returns an array of messages
+        String [] fields = fileContent.split("(\\r\\n\\s|\\r\\s|\\n\\s)");
 
-        System.out.print(Arrays.toString(fields));
+        for (String field : fields) {
+            //System.out.println(field);
+
+            Scanner scanner = new Scanner( field ).useDelimiter("\\n");
+            String TIME_STAMP = scanner.next(); // 4231
+            String NICK_NAME = scanner.next(); // "Java Programming"
+            String CONTENT = scanner.next();  // 1000.00
+
+            System.out.println("[" + TIME_STAMP + "]" + NICK_NAME + ":" + CONTENT);
+        }
+
+
+        //System.out.print(Arrays.toString(fields));
         return fileContent;
     }
 }
