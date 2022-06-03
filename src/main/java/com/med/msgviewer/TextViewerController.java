ceb9ce767f3ca96496028fe23a15e80df2cc88ca
Feynman43 <mohammed.znitar@praguecollege.cz>
2022-06-03 02:14:50 +0200

    Connecting newly added components to controller

--- src/main/java/com/med/msgviewer/TextViewerController.java
+++ src/main/java/com/med/msgviewer/TextViewerController.java
@@ -2,8 +2,11 @@ package com.med.msgviewer;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class TextViewerController {
+    public Label PathOfFile;
+    public TextArea FormattedTextArea;
     @FXML
     private Label welcomeText;
 
