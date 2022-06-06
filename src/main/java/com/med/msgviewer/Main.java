e179dd43c823f8dfab417b274afd89057ae6002d
Feynman43 <mohammed.znitar@praguecollege.cz>
2022-06-06 13:00:38 +0200

    Setting the minimum sizeof the window

--- src/main/java/com/med/msgviewer/Main.java
+++ src/main/java/com/med/msgviewer/Main.java
@@ -14,6 +14,10 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        stage.setTitle("Message Viewer");
+        stage.setMinHeight(250);
+        stage.setMinWidth(500);
         stage.setScene(scene);
         stage.show();
     }
